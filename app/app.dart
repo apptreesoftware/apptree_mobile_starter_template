8eed2cebe66fa7822c8a14660991dc8017127cb3
Matthew Smith <matthew.smith@apptreesoftware.com>
2025-03-20 10:49:43 -0700

    Initial commit

---
+++ app/app.dart
@@ -0,0 +1,3 @@
+import 'package:apptree_dart_sdk/apptree.dart';
+
+var app = App(name: 'Apptree Mobile Starter Template', configVersion: 2);
---
+++ main.dart
@@ -0,0 +1,7 @@
+// IMPORTANT: This file is auto-generated. Do not edit it directly.
+
+import 'app/app.dart';
+
+void main() {
+  app.initialize();
+}
